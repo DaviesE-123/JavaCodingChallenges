3ec4db7265658a4936fd0fdcc3da2c99571bb934
eughan davies <Davies.eughan@gmail.com>
2024-03-09 12:53:18 +0000

    I am adding a readinput for an integer

---
+++ src/ReadInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ReadInput {
+
+    public static void main(String[] args) {
+        System.out.println("Input an integer");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+    }
+}
